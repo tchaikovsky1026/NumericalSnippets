e6c62c24e4835dc5b16b03aace53297009ab4dfd
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-12-30 19:25:48 +0900

    create: Trigonometryクラス

---
+++ src/snippet/num/Trigonometry.java
@@ -0,0 +1,20 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ * See the UNLICENSE file for details.
+ */
+
+/*
+ * 2025.12.30
+ */
+package snippet.num;
+
+/**
+ * 三角関数の計算に関わる数値計算スニペット.
+ */
+public final class Trigonometry {
+
+    private Trigonometry() {
+        // インスタンス化不可
+        throw new AssertionError();
+    }
+}
