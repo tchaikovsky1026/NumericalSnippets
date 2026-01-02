aa3c9236c8b5e26a9f9279e5610b71be77873e97
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-01-02 18:20:08 +0900

    add: Exponentiation

---
+++ src/snippet/num/Exponentiation.java
@@ -0,0 +1,20 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ * See the UNLICENSE file for details.
+ */
+
+/*
+ * 2026.1.2
+ */
+package snippet.num;
+
+/**
+ * べき乗, 指数対数の計算に関わる数値計算スニペット.
+ */
+public final class Exponentiation {
+
+    private Exponentiation() {
+        // インスタンス化不可
+        throw new AssertionError();
+    }
+}
