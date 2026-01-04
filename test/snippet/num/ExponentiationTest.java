81d239c92a6c16c506aab48607613159ad00210a
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-01-04 14:40:07 +0900

    add,test: logSumExpのテストを修正

--- test/snippet/num/ExponentiationTest.java
+++ test/snippet/num/ExponentiationTest.java
@@ -5,11 +5,14 @@
 
 package snippet.num;
 
+import static java.util.stream.Collectors.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 import static snippet.num.Exponentiation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -30,6 +33,69 @@ final class ExponentiationTest {
 
     @RunWith(Theories.class)
     public static class logSumExpのサイズバリエーションテスト {
+
+        @DataPoints
+        public static int[] sizes = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        @Theory
+        public void test_サイズパラメトリック(int size) {
+
+            final int iteration = 20;
+            for (int c = 0; c < iteration; c++) {
+                double[] arr = new double[size];
+                for (int i = 0; i < size; i++) {
+                    arr[i] = 500 * (ThreadLocalRandom.current().nextDouble() * 2d - 1d);
+                }
+
+                compareAndAssert(logSumExpRef(arr), logSumExp(arr));
+            }
+        }
+    }
+
+    @RunWith(Theories.class)
+    public static class logSumExpの特殊値のサイズバリエーションテスト {
+
+        @DataPoints
+        public static int[] sizes = { 0, 1, 2, 3, 4 };
+
+        @DataPoints
+        public static double[][] data_special = {
+                {
+                        Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY,
+                        Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY
+                },
+                {
+                        Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY,
+                        Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY },
+                { Double.NaN, 1, 1, 1 },
+                { 1, Double.NaN, 1, 1 },
+                { 1, 1, Double.NaN, 1 },
+                { 1, 1, 1, Double.NaN }
+        };
+
+        @Theory
+        public void test_サイズパラメトリック(double[] arrX, int size) {
+            double[] xs = Arrays.copyOf(arrX, size);
+
+            final int iteration = 20;
+            for (int c = 0; c < iteration; c++) {
+                // arr = shaffled xs 
+                List<Double> xsList = new ArrayList<>(
+                        Arrays.stream(xs)
+                                .mapToObj(d -> d)
+                                .collect(toList()));
+                Collections.shuffle(xsList);
+                double[] arr = xsList.stream()
+                        .mapToDouble(d -> d)
+                        .toArray();
+
+                compareAndAssert(logSumExpRef(arr), logSumExp(arr));
+            }
+        }
+    }
+
+    @RunWith(Theories.class)
+    public static class logSumExpのリファレンスのサイズバリエーションテスト {
 
         @DataPoints
         public static List<double[]> data;
@@ -73,7 +139,7 @@ final class ExponentiationTest {
         public void test_サイズ0(double[] arrX) {
             double expected = Double.NEGATIVE_INFINITY;
 
-            compareAndAssert(expected, logSumExp());
+            compareAndAssert(expected, logSumExpRef());
         }
 
         @Theory
@@ -81,7 +147,7 @@ final class ExponentiationTest {
             double x1 = arrX[0];
 
             double expected = x1;
-            compareAndAssert(expected, logSumExp(x1));
+            compareAndAssert(expected, logSumExpRef(x1));
         }
 
         @Theory
@@ -90,8 +156,8 @@ final class ExponentiationTest {
             double x2 = arrX[1];
 
             double expected = Math.log(Math.exp(x1) + Math.exp(x2));
-            compareAndAssert(expected, logSumExp(x1, x2));
-            compareAndAssert(expected, logSumExp(x2, x1));
+            compareAndAssert(expected, logSumExpRef(x1, x2));
+            compareAndAssert(expected, logSumExpRef(x2, x1));
         }
 
         @Theory
@@ -101,9 +167,9 @@ final class ExponentiationTest {
             double x3 = arrX[2];
 
             double expected = Math.log(Math.exp(x1) + Math.exp(x2) + Math.exp(x3));
-            compareAndAssert(expected, logSumExp(x1, x2, x3));
-            compareAndAssert(expected, logSumExp(x1, x3, x2));
-            compareAndAssert(expected, logSumExp(x3, x2, x1));
+            compareAndAssert(expected, logSumExpRef(x1, x2, x3));
+            compareAndAssert(expected, logSumExpRef(x1, x3, x2));
+            compareAndAssert(expected, logSumExpRef(x3, x2, x1));
         }
 
         @Theory
@@ -115,11 +181,86 @@ final class ExponentiationTest {
 
             double expected = Math.log(
                     Math.exp(x1) + Math.exp(x2) + Math.exp(x3) + Math.exp(x4));
-            compareAndAssert(expected, logSumExp(x1, x2, x3, x4));
-            compareAndAssert(expected, logSumExp(x1, x2, x4, x3));
-            compareAndAssert(expected, logSumExp(x1, x3, x4, x2));
-            compareAndAssert(expected, logSumExp(x2, x3, x4, x1));
+            compareAndAssert(expected, logSumExpRef(x1, x2, x3, x4));
+            compareAndAssert(expected, logSumExpRef(x1, x2, x4, x3));
+            compareAndAssert(expected, logSumExpRef(x1, x3, x4, x2));
+            compareAndAssert(expected, logSumExpRef(x2, x3, x4, x1));
         }
+    }
+
+    private static double logSumExpRef(double... x) {
+
+        if (x.length == 0) {
+            return Double.NEGATIVE_INFINITY;
+        }
+
+        /*
+         * 相対的大きさでexp(-46)未満は0とする.
+         */
+        final double LOG_SUM_EXP_THRESHOLD = -46;
+
+        /**
+         * exp( LOG_SUM_EXP_THRESHOLD )
+         */
+        final double SUM_EXP_THRESHOLD = 1.0530617357553812378763324449428E-20;
+
+        /*
+         * 1周のループで終わらせられるようにする.
+         * 逐次的に改善するように修正する (maxXの逐次改善)
+         */
+
+        double maxX = x[0];
+        int len = x.length;
+        int indMax = 0;
+        for (int j = 1; j < len; j++) {
+            double x_j = x[j];
+            if (x_j > maxX) {
+                maxX = x_j;
+                indMax = j;
+            }
+        }
+        if (!Double.isFinite(maxX)) {
+            return maxX;
+        }
+
+        /*
+         * log(exp(max) + exp(min1) + exp(min2) + ...)
+         * = max + log(1 + exp(min1 - max) + exp(min2 - max) + ...)
+         * 
+         * ただし, |max| >= 1ならば,
+         * exp(min1 - max) + exp(min2 - max) + ... < exp(-46)
+         * の場合は log(1 + exp(min1 - max) + exp(min2 - max) + ...) は無視できる.
+         * 
+         * exp(min1 - max) + exp(min2 - max) + ... < exp(-46)
+         * でない場合でも,
+         * exp(min[] - max) < exp(-46)
+         * ならばその項は無視できる.
+         */
+        if (Math.abs(maxX) <= 1d) {
+            double sum = 0;
+            for (int j = 0; j < indMax; j++) {
+                sum += Math.exp(x[j] - maxX);
+            }
+            for (int j = indMax + 1; j < len; j++) {
+                sum += Math.exp(x[j] - maxX);
+            }
+            return maxX + Math.log1p(sum);
+        }
+
+        double sum = 0;
+        for (int j = 0; j < indMax; j++) {
+            double xjMinusMax = x[j] - maxX;
+            double e1 = xjMinusMax < LOG_SUM_EXP_THRESHOLD ? 0 : Math.exp(xjMinusMax);
+            sum += e1;
+        }
+        for (int j = indMax + 1; j < len; j++) {
+            double xjMinusMax = x[j] - maxX;
+            double e1 = xjMinusMax < LOG_SUM_EXP_THRESHOLD ? 0 : Math.exp(xjMinusMax);
+            sum += e1;
+        }
+        return sum < SUM_EXP_THRESHOLD
+                ? maxX
+                : maxX + Math.log1p(sum); //NaNは最悪この行に到達
     }
 
     /**
